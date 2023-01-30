f4b7943937e3a37038a5c845d44e0eec35df3d78
Tiago Ribeiro <tiago.d.ribeiro@hotmail.com>
2023-01-30 00:14:01 +0000

    updated routing

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,14 @@ import { handlePost } from "../controllers/handlePost.js";
 
 const router = express.Router();
 
+router.get("/version", (req, res) => {
+   res.send("Email API v2");
+});
+
 router.get("/status", (req, res) => {
    res.send("OK");
 });
 
-router.post("/send", handlePost);
+router.post("/v2/send", handlePost);
 
 export default router;
--- server.js
+++ server.js
@@ -8,11 +8,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use("/api/v2", routes);
-
-app.get("/api/version", (req, res) => {
-   res.send("Email API v2");
-});
+app.use("/api", routes);
 
 app.listen(port, () => {
    console.log(`Email REST API listening on port ${port}!`);

043dd1835f483db87f8ce93062f0f1bdf21222bb
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-05-18 16:14:28 +0100

    increased limit size

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = config.port;
 
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", routes);
